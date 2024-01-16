ae169a8f9d05ea623552909836f8275ed0138774
parsilver <magician_canobi@hotmail.com>
2024-01-16 13:15:42 +0700

    Refactor in PromptRepository

--- src/Resources/PromptRepository.php
+++ src/Resources/PromptRepository.php
@@ -11,15 +11,9 @@ class PromptRepository implements PromptRepositoryInterface
      */
     public function compile(string $name, array $bindings = []): string
     {
-        if (! file_exists(__DIR__."/../../stubs/prompts/{$name}.txt")) {
-            return str_replace(
-                array_map(fn ($key) => ":{$key}:", array_keys($bindings)),
-                array_values($bindings),
-                $name,
-            );
-        }
-
-        $content = file_get_contents(__DIR__."/../../stubs/prompts/{$name}.txt");
+        $content = file_exists(__DIR__."/../../stubs/prompts/{$name}.txt")
+            ? file_get_contents(__DIR__."/../../stubs/prompts/{$name}.txt")
+            : $name;
 
         return str_replace(
             array_map(fn ($key) => ":{$key}:", array_keys($bindings)),
